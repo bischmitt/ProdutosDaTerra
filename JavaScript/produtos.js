1cdaaf23be58881098f5596e85fa9e47c4f62ccc
Edufranco <edufranco.dc@gmail.com>
2021-10-22 23:31:10 -0300

    construindo classe carrinho

--- JavaScript/produtos.js
+++ JavaScript/produtos.js
@@ -7,5 +7,95 @@ class Modal {
     }
 
 }
-let modal = new Modal
-$(".carrinho a").click(modal.abrirModal())
+
+let modal = new Modal()
+
+$("#abrirModal").click(function(){
+    modal.abrirModal()
+})
+
+$("#but1").click(function(){
+    modal.fecharModal()
+})
+
+class Produto {
+    constructor(){
+        this.todosOsProdutos = [
+            {
+                id: 1,
+                nome: "Banana",
+                preco: 5.50,
+                quantidade: 0.5,
+
+            },
+            {
+                id: 2,
+                nome: "Chuchu",
+                preco: 3.50,
+                quantidade: 0.5,
+
+            },
+            {
+                id: 3,
+                nome: "Maracujá",
+                preco: 8.00,
+                quantidade: 0.5,
+
+            },
+            {
+                id: 4,
+                nome: "Pimentão Amarelo",
+                preco: 5.50,
+                quantidade: 0.5,
+
+            },
+            {
+                id: 5,
+                nome: "Pimentão Verde",
+                preco: 4.25,
+                quantidade: 0.5,
+
+            },
+            {
+                id: 6,
+                nome: "Pimentão Vermelho",
+                preco: 5.75,
+                quantidade: 0.5,
+
+            },
+            {
+                id: 7,
+                nome: "Pitaya Branca",
+                preco: 8.50,
+                quantidade: 0.5,
+
+            },
+            {
+                id: 8,
+                nome: "Pitaya Rosa",
+                preco: 10.20,
+                quantidade: 0.5,
+
+            },
+            {
+                id: 9,
+                nome: "Quiabo",
+                preco: 5.50,
+                quantidade: 0.5,
+
+            },
+        ]
+        this.itensDoCarrinho = new Array()
+    }
+    enviarParaCarrinho(idEscolhido){
+        this.todosOsProdutos.forEach(produto =>{
+            if(produto.id == idEscolhido){
+                let quantidadeDoProdutoEscolhido = $(".item" + idEscolhido + " .pesokg").val()
+                produto.quantidade = Number(quantidadeDoProdutoEscolhido)
+                this.itensDoCarrinho.push(this.todosOsProdutos[idEscolhido - 1])
+            }
+        })
+    }
+}
+let produto = new Produto()
+console.log(produto.itensDoCarrinho)
