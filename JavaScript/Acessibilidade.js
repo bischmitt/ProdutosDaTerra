e99ebb4ec24300bd6300eb2d8f6f3e4b57852947
Mariana <mnp1807@hotmail.com>
2021-10-27 10:38:12 -0300

    adicionei botão baixa visão

---
+++ JavaScript/Acessibilidade.js
@@ -0,0 +1,23 @@
+// Criei uma função fonte para alterar a acessibilidade
+function fonte(e) {
+// criei uma var e chamei minha class acessibilidade por jQuery
+	var elemento = $(".acessibilidade");
+	var fonte = parseInt(elemento.css('font-size'));
+
+	var body = $("body");
+	const fonteNormal = parseInt(body.css('font-size'));
+
+// Fiz por if (se) as funções de cada butão
+	if (e == 'a') {
+		fonte++;
+	}
+	if (e == 'd'){
+		fonte--;
+	}
+	if (e == 'n'){
+		fonte = fonteNormal;
+	}
+// cetei o css para alterar o fonte size (tamanho) na nossa função fonte
+	elemento.css("fontSize", fonte);
+	
+}
