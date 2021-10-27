36f48568414ce4fa798017ee411f14bbbe72d329
Mariana <mnp1807@hotmail.com>
2021-10-27 08:00:06 -0300

    comentei os códigos e inseri botão

--- JavaScript/Acessibilidade.js
+++ JavaScript/Acessibilidade.js
@@ -1,12 +1,13 @@
+// Criei uma função fonte para alterar a acessibilidade
 function fonte(e) {
-
+// criei uma var e chamei minha class acessibilidade por jQuery
 	var elemento = $(".acessibilidade");
 	var fonte = parseInt(elemento.css('font-size'));
 
 	var body = $("body");
 	const fonteNormal = parseInt(body.css('font-size'));
 
-
+// Fiz por if (se) as funções de cada butão
 	if (e == 'a') {
 		fonte++;
 	}
@@ -16,7 +17,7 @@ function fonte(e) {
 	if (e == 'n'){
 		fonte = fonteNormal;
 	}
-
+// cetei o css para alterar o fonte size (tamanho) na nossa função fonte
 	elemento.css("fontSize", fonte);
 	
 }
